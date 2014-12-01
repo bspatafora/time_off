e0a095321edf68e8194a00e27d3fea3ed9b0f265
bspatafora <benjamin.spatafora@gmail.com>
2014-12-01 11:24:31 -0600

    Use SendGrid for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,16 @@ Rails.application.configure do
   config.log_level = :info
   config.log_formatter = ::Logger::Formatter.new
 
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :authentication       => :plain,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :domain               => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   config.i18n.fallbacks = true
 
